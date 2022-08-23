ff51a685bf0a260c465209facc60c68180c2a8fa
Yash Patel <ympatel132635@gmail.com>
2022-08-23 12:43:58 +0530

    Done

--- src/main/kotlin/practical1.kt
+++ src/main/kotlin/practical1.kt
@@ -1,21 +1,21 @@
 fun main(){
-    var a: Int= 36
-    var b: Double=20.12
-    var c: Float= 1.5F
-    var d: Long = 12135485
-    var e: Short = -2
-    var f: Byte = 127
-    var g: Char = 'Y'
-    var h: Boolean = true
-    var i: String = "Yash"
-
+    var a = 36
     println("Integer Value : $a")
+    var b =20.1223
     println("Double Value : $b")
+    var c = 1.55F
     println("Float Value : $c")
+    var d = 12135485
     println("Long Value : $d")
+    var e = -2
     println("Short Value : $e")
+    var f = 127
     println("Byte Value : $f")
+    var g = 'Y'
     println("Char Value : $g")
+    var h = true
     println("Boolean Value : $h")
+    var i = "Yash"
     println("String Value : $i")
+
 }
